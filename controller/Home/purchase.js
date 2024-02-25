524e16f239ee252b0b93d99bc964a0e045527b5e
Vanshika Rajput <vanshikavce19@gmail.com>
2024-02-25 20:32:59 +0530

    purchaseHalf

--- controller/Home/purchase.js
+++ controller/Home/purchase.js
@@ -1,33 +1,53 @@
 const User = require("../../models/user");
+const multer = require("multer");
+const path = require("path");
+
+// Set up multer storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../uploads"); // Uploads will be stored in the 'uploads/' directory
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname); // Generate a unique filename
+  },
+});
+
+const upload = multer({ storage: storage }).single("image");
 
 exports.addBill = async (req, res) => {
   try {
-    const { userId, vendorName, challanNumber, orderDate, dueDate, remark } =
-      req.body;
+    upload(req, res, async (err) => {
+      if (err) {
+        return res.status(400).json({ error: "Error uploading image" });
+      }
 
-    // Find the user by ID
-    const user = await User.findById(userId);
+      const { userId, vendorName, challanNumber, orderDate, dueDate, remark } =
+        req.body;
 
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
+      // Find the user by ID
+      const user = await User.findById(userId);
 
-    // Initialize the additem array if it's not defineds
-    user.bill = user.bill || [];
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
 
-    user.bill.push({
-      vendorName,
-      challanNumber,
-      orderDate,
-      dueDate,
-      remark,
-    });
+      user.bill = user.bill || [];
 
-    await user.save();
+      user.bill.push({
+        vendorName,
+        challanNumber,
+        orderDate,
+        dueDate,
+        remark,
+        image: req.file.filename, // Save the filename in the bill object
+      });
 
-    return res.status(200).json({
-      success: true,
-      message: "Bill added successfully",
+      await user.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Bill added successfully",
+      });
     });
   } catch (error) {
     console.error(error);
