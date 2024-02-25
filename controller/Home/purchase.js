be148ed7cba221b52ec34ceb5007dc63b7293e6f
mediveda <you@example.com>
2024-02-25 20:34:00 +0530

    ok

--- controller/Home/purchase.js
+++ controller/Home/purchase.js
@@ -9,7 +9,7 @@ exports.addBill = async (req, res) => {
     const user = await User.findById(userId);
 
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: "User not " });
     }
 
     // Initialize the additem array if it's not defineds
